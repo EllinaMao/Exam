87fd6c1c17ae84b4bd3835c1bab4ad9de8ce1dba
Olga <xotaoshi@gmail.com>
2025-08-09 19:44:20 +0300

    Add project files.

---
+++ Tempfiles t1/DictionaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempfiles_t1
+{
+    public class DictionaryEntry
+    {
+
+        public string Word { get; set; }
+        public List<string> Translations { get; set; } = new();
+
+    }
+}
