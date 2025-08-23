80480202ec784c642d62f6d144c11083678a375a
Olga <xotaoshi@gmail.com>
2025-08-23 21:42:43 +0300

    выфвф

--- GuessGame/Quiz.cs
+++ GuessGame/Quiz.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace GuessGame
 {
-    public class MathQuiz
+    public class Quiz:IQuiz
     {
-        public string Title => "Math Quiz";
+        public string Title { get; set; }
         public string Description => "A quiz to test your math skills.";
         public string Category => "Mathematics";
         public Dictionary<string, List<string>>? Questions { get; set; }
--- GuessGame/QuizFactory.cs
+++ GuessGame/QuizFactory.cs
@@ -12,15 +12,16 @@ namespace GuessGame
 
         static QuizFactory()
         {
+            //Берем все типы (классы, интерфейсы, структуры) из сборки, в которой сейчас выполняется код
             quizTypes = Assembly.GetExecutingAssembly()
-                .GetTypes()
-                .Where(t => typeof(IQuiz).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                .GetTypes()//Фильтрует только те типы, которые:не являются интерфейсом и не абстрактный класс
+                .Where(t => typeof(IQuiz).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)//
                 .Select(t => (IQuiz)Activator.CreateInstance(t)!)
-                .ToDictionary(q => q.Title, q => q.GetType() , StringComparer.OrdinalIgnoreCase);
+                .ToDictionary(q => q.Title, q => q.GetType(), StringComparer.OrdinalIgnoreCase);
         }
         public static IQuiz CreateQuiz(string quizType)
         {
-            if (quizTypes.TryGetValue(quizType , out var type))
+            if (quizTypes.TryGetValue(quizType, out var type))
                 return (IQuiz)Activator.CreateInstance(type)!;
 
             throw new ArgumentException($"Викторина '{quizType}' не найдена.");
